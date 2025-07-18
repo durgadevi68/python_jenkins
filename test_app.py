017049424a3c569c82541180895b2f28e23f4819
durgadevi68 <50898231+durgadevi68@users.noreply.github.com>
2025-07-18 13:09:34 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,5 +5,5 @@ class TestApp(unittest.Testcase):
     def test_hello(self):
         self.assertEqual(hello(), "Hello, Jenkins!")
 
-if __name__ = "__main__":
-    unittest(main):
+if __name__ == "__main__":
+     unittest(main):
